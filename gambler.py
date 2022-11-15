0fe6bcd8ea227979ec5cee51af084fdb1dc0772f
Shubham Patel <shubhamprecious1@gmail.com>
2022-11-15 13:07:39 +0530

    Add files via upload

---
+++ gambler.py
@@ -0,0 +1,11 @@
+initial_capital = input('Enter initial Capital: ')
+
+winning_prob = input('Enter winning probability: ')
+
+betting_amount = input('Enter betting amount: ')
+
+risk_to_reward = input('Enter risk to reward ratio: ')
+
+avg_lose = input('Enter average lose factor in one instance: ')
+
+current_capital = initial_capital 
