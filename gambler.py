c93c8a536eff7a3ad38b14f6a29ce378eed61b13
Shubham Patel <shubhamprecious1@gmail.com>
2022-11-16 07:35:25 +0530

    Update gambler.py

--- gambler.py
+++ gambler.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-initial_capital = float(input('Enter initial Capital: '))
-
-winning_prob = float(input('Enter winning probability: '))
-
-betting_amount = float(input('Enter betting amount: '))
-
-reward_ratio = float(input('Enter reward ratio: '))
-
-avg_lose = float(input('Enter average lose factor in one instance: '))
-
-if trade == win
-
-#current_capital = betting_amount*((1+reward_ratio)**winning_prob)*((1-avg_lose)**(1-winning_prob))
-
-print(current_capital)
-=======
- 
 import random
 initial_capital=float(input('EnterinitialCapital:'))
 winning_prob=float(input('Enterwinningprobability:'))
