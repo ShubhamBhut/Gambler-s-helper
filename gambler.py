07bdc5034f078b0e8496347be2b9d242abc76409
Shubham Patel <shubhamprecious1@gmail.com>
2022-11-16 07:39:30 +0530

    Update gambler.py

--- gambler.py
+++ gambler.py
@@ -9,10 +9,15 @@ avg_lose=float(input('Enter averagelosefactorinloneinstance:'))
  
 trade = random.choices([True, False], weights=[winning_prob, 1-winning_prob], k=2)
  
-if trade is True:
-    current_capital = betting_amount * reward_ratio
-else:
- 	current_capital = betting_amount - (betting_amount* avg_lose)
+no_of_trades = int(input("Enter no. of trades: "))
+
+for i in range no_of_trades:
+    trade = random.choices([True, False], weights=[winning_prob, 1-winning_prob], k=2)
+ 
+    if trade is True:
+        current_capital = betting_amount * reward_ratio
+    else:
+ 	    current_capital = betting_amount - (betting_amount* avg_lose)
  	
 print(current_capital)
 
