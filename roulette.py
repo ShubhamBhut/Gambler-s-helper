1177038b04c9a37baa12ae132c757298e55c90d3
Shubham Patel <shubhamprecious1@gmail.com>
2022-11-23 16:25:52 +0530

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -3,4 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-total_numbers = 
+european_roulette = []
+american_roulette = ["double zero"]
+for i in range(37):
+    european_roulette.append(i)
+    american_roulette.append(i)
+
+print(american_roulette)
+print(european_roulette)
+
