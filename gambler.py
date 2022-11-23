aa3721f0f2f71dd56763bd09310302dbf8e16e59
Shubham Patel <shubhamprecious1@gmail.com>
2022-11-23 07:20:02 +0530

    Update gambler.py

--- gambler.py
+++ gambler.py
@@ -1,18 +1,18 @@
 import random
-initial_capital=float(input('EnterinitialCapital:'))
-winning_prob=float(input('Enterwinningprobability:'))
+initial_capital=float(input('Enter initial Capital:'))
+winning_prob=float(input('Enter winning probability:'))
  
-betting_amount=float(input('Enterbettingamount:'))
+betting_amount=float(input('Enter betting-amount:'))
   
-reward_ratio=float(input('Enterrisktorewardratio:'))
-avg_lose=float(input('Enter averagelosefactorinloneinstance:'))
+reward_ratio=float(input('Enter reward ratio:'))
+avg_lose=float(input('Enter average lose-factor in one instance:'))
  
 trade = random.choices([True, False], weights=[winning_prob, 1-winning_prob], k=1)
  
 no_of_trades = int(input("Enter no. of trades: "))
 
 for i in range(no_of_trades):
-    trade = random.choices([True, False], weights=[winning_prob, 1-winning_prob], k=2)
+    trade = random.choices([True, False], weights=[winning_prob, 1-winning_prob], k=1)
  
     if trade is True:
         current_capital = betting_amount * reward_ratio
