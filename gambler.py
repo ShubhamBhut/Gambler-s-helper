6034fcdf25d29fc90cd8bfdeb1816b59bf13e0e4
Shubham Patel <shubhamprecious1@gmail.com>
2022-11-23 07:30:31 +0530

    Update gambler.py

--- gambler.py
+++ gambler.py
@@ -11,13 +11,15 @@ trade = random.choices([True, False], weights=[winning_prob, 1-winning_prob], k=
  
 no_of_trades = int(input("Enter no. of trades: "))
 
+current_capital = initial_capital
+
 for i in range(no_of_trades):
     trade = random.choices([True, False], weights=[winning_prob, 1-winning_prob], k=1)
  
     if trade[0] == True:
-        current_capital = betting_amount * reward_ratio
+        current_capital = current_capital + (betting_amount * reward_ratio)
     else:
- 	    current_capital = betting_amount - (betting_amount* avg_lose)
+ 	    current_capital = current_capital - (betting_amount * avg_lose)
     print(current_capital)
  	
 print(current_capital)
