c885c02ec5fccb675935431df1b16537b4cea777
EmulatedDragon <79074183+EmulatedDragon@users.noreply.github.com>
2022-11-24 10:30:29 +0530

    FIRST functinoal multi bettable cli release

--- roulette_game.py
+++ roulette_game.py
@@ -1,28 +1,42 @@
 import random
 import math
 
-money=int(input('Enter the betting amount: '))
-type=input('type of bet, for single enter 1, for 12 enter 2, for odd/even enter 3, for high/low, enter 4: ').split()
+continue_roulette=True
 
-print(type)
-if '1' in type:
-    single=input('bet on').split() #1-36
+input_arr=[]
 
-if '2' in type:
-    dozen=input('first,second,third').split() #1,2,3
+money=int(input('enter total money: '))
 
-if '3' in type:
-    oddeven=input('odd or even').split() #1,2
+while(continue_roulette):
 
-if '4' in type:
-    half=input('first,second').split() #1,2
+    type=input('type of bet, for bet enter 1, for 12 enter 2, for odd/even enter 3, for high/low, enter 4: ').split()
+    
+    if '1' in type:
+        bet=input('bet on: ') #1-36
+
+    if '2' in type:
+        bet=input('first,second,third: ') #1,2,3
+
+    if '3' in type:
+        bet=input('odd or even: ') #1,2
+
+    if '4' in type:
+        bet=input('first,second: ') #1,2
+
+    betting_amount=int(input('Enter the betting amount: '))
+
+    input_arr.append([betting_amount,type,bet])
+
+    temp=input('one more? Y/N: ')
+
+    if temp=='N' or temp=='n':continue_roulette=False
 
 
 roll=random.randint(1, 36)
 
 print('roll is ', roll)
 
-rolldozen=str(math.ceil(roll/12))
+rollbet=str(math.ceil(roll/12))
 
 if roll%2==1: rolloddeven=1
 else: rolloddeven=2
@@ -30,33 +44,41 @@ else: rolloddeven=2
 if roll<19: rolllowhigh=1
 else: rolllowhigh=2
 
+roll = str(roll)
+rollbet = str(rollbet)
 rolloddeven = str(rolloddeven)
 rolllowhigh = str(rolllowhigh)
-#payout for type 1
 
-if '1' in type:
-    payout = (roll.count(single)*35*money) - (len(roll) - roll.count(single)*money)
-    money=money+payout
-
-#payout for type 2
-
-if '2' in type:
-    payout = (dozen.count(rolldozen) * 2 * money) - ((len(dozen)-dozen.count(rolldozen)) * money)
-    money=money+payout
-
-#payout fo type 3
-
-if '3' in type:
-    payout = (rolloddeven.count(oddeven)*1*money) - ((len(rolloddeven) - rolloddeven.count(oddeven))*money)
-    money=money+payout
-
-#payout for type 4
-
-if '4' in type:
-    payout = (rolllowhigh.count(half)*1*money) - ((len(rolllowhigh) - rolllowhigh.count(half)) * money)
-    money=money+payout
+for [betting_amount,type,bet] in input_arr:
+    #payout for type 1
+    if '1' in type:
+        if bet==roll:
+            payout = 35*betting_amount
+            money+=payout
+        else:
+            money-=betting_amount
+    #payout for type 2
+    if '2' in type:
+        if bet==rollbet:
+            payout = 2*betting_amount
+            money+=payout
+        else:
+            money-=betting_amount
+    #payout fo type 3
+    if '3' in type:
+        if bet==rolloddeven:
+            payout = betting_amount
+            money+=payout
+        else:
+            money-=betting_amount
+    #payout for type 4
+    if '4' in type:
+        if bet==rolllowhigh:
+            payout = 1*betting_amount
+            money+=payout
+        else:
+            money-=betting_amount
 
 print('Final money is ', money)
-print('Type is: ', type)
-print('dozen is: ', dozen)
-print(rolldozen)
+
+#SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH
